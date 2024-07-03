7793600c6c16c9c786d2aa7735993862f44cd5a9
Oleksandr Moroz <molecxander@gmail.com>
2024-07-03 19:36:37 +0300

    Solution

--- routers/temperatures.py
+++ routers/temperatures.py
@@ -1,3 +1,4 @@
+import os
 from datetime import datetime
 from typing import List
 
@@ -26,7 +27,11 @@ def get_db():
 
 
 @router.get("/temperatures/", response_model=List[Temperature])
-def read_temperatures(skip: int = 0, limit: int = 10, db: Session = Depends(get_db)):
+def read_temperatures(
+        skip: int = 0,
+        limit: int = 10,
+        db: Session = Depends(get_db)
+):
     temperatures = get_temperatures(db, skip=skip, limit=limit)
     return temperatures
 
@@ -35,22 +40,33 @@ def read_temperatures(skip: int = 0, limit: int = 10, db: Session = Depends(get_
 def read_temperatures_by_city(
     city_id: int, skip: int = 0, limit: int = 10, db: Session = Depends(get_db)
 ):
-    temperatures = get_temperatures_by_city(db, city_id=city_id, skip=skip, limit=limit)
+    temperatures = get_temperatures_by_city(
+        db,
+        city_id=city_id,
+        skip=skip,
+        limit=limit
+    )
     return temperatures
 
 
 @router.post("/temperatures/update")
 async def update_temperatures(db: Session = Depends(get_db)):
+    api_key = os.environ.get("API_KEY")
     cities = get_cities(db)
     async with httpx.AsyncClient() as client:
         for city in cities:
+            params = {
+                "key": api_key,
+                "q": city.name
+            }
             response = await client.get(
-                f"https://api.weatherapi.com/v1/current.json?key=YOUR_API_KEY&q={city.name}"
-            )
+                "https://api.weatherapi.com/v1/current.json", params=params)
             data = response.json()
             temperature = data["current"]["temp_c"]
             temp_data = TemperatureCreate(
-                city_id=city.id, date_time=datetime.now(), temperature=temperature
+                city_id=city.id,
+                date_time=datetime.now(),
+                temperature=temperature
             )
             create_temperature(db=db, temperature=temp_data)
     return {"status": "temperatures updated"}
